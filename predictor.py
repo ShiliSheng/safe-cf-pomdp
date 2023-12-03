eea389872b20a0c81e4ff3009a6d476a44c83f2a
sheng sheng <shengsheng@shengs-MacBook-Pro-2.local>
2023-12-02 19:55:57 -0500

    update predition by current time

--- predictor.py
+++ predictor.py
@@ -8,17 +8,9 @@ import torch.nn as nn
 from torch.utils.data import DataLoader, TensorDataset
 import torch.optim as optim
 from sklearn.model_selection import train_test_split
+import pickle
+import time
 
-path = './OpenTraj/datasets/ETH/seq_eth/'
-LSTM_file = path + '/model_weights.pth'
-input_size = 2  # Assuming 2 features (x, y) per timestep
-hidden_size = 64
-output_size = 2  # Assuming 2 features in the output
-
-history_length = 5
-prediction_length = 2
-
-# Assuming you have a simple LSTM model
 class LSTMModel(nn.Module):
     def __init__(self, input_size, hidden_size, output_size):
         super(LSTMModel, self).__init__()
@@ -30,115 +22,218 @@ class LSTMModel(nn.Module):
         output = self.fc(lstm_out[:, -1, :])  # Assuming you want to predict from the last timestep
         return output
 
-class Predictor():
-    def __init__(self, history_length, prediction_legnth, input_size):
+class TrainModel:
+    def __init__(self, path, filename) -> None:
+        self.input_size = 2  # Assuming 2 features (x, y) per timestep
+        self.hidden_size = 64
+        self.output_size = 2  # Assuming 2 features in the output
+        self.history_length = 4
+        self.prediction_length = 2
+        self.path = './OpenTraj/datasets/ETH/seq_eth/'
+        self.filename = filename
+
+    def preprocess(self, ):
+        path = self.path
+        file = self.path + self.filename
+        # file = 
+        csv_columns = ["frame_id", "id", "x", "z", "y", "vel_x", "vel_z", "vel_y"]
+        # read from csv => fill traj table
+        raw_dataset = pd.read_csv(file, sep=r"\s+", header=None, names=csv_columns)
+        raw_dataset["timestamp"]= raw_dataset.frame_id
+        start_frame = raw_dataset.frame_id.min()
+        d_frame = np.diff(pd.unique(raw_dataset["frame_id"]))
+        fps = d_frame[0] * 2.5  # 2.5 is the common annotation
+        for i in raw_dataset.index:
+            raw_dataset.loc[i, "timestamp"] = (raw_dataset.loc[i, "frame_id"] - start_frame) / fps
+        ####### raw_dataset['pos'] = raw_dataset.apply(lambda row: [row['x'], row['y']], axis=1)
+        raw_dataset = raw_dataset.loc[:, ["timestamp", "id", "x", "y"]]
+
+        train_id, test_id = train_test_split(raw_dataset.id.unique(), test_size= 1 / 10, random_state=42)
+        train_id, validation_id = train_test_split(train_id, test_size = 0.2, random_state = 42)
+        train_id, validation_id, test_id = set(train_id), set(validation_id), set(test_id)
+
+        train_raw_dataset = raw_dataset[raw_dataset.id.isin(train_id)]
+        validation_raw_dataset = raw_dataset[raw_dataset.id.isin(validation_id)]
+        test_raw_dataset = raw_dataset[raw_dataset.id.isin(test_id)]
+
+        train_raw_dataset.to_csv(path + "raw_train.csv")
+        validation_raw_dataset.to_csv(path + "raw_validation.csv")
+        test_raw_dataset.to_csv(path + "raw_test.csv")
+
+        reshaped_dataset = test_raw_dataset.pivot_table(index='timestamp',
+                                                    columns='id',
+                                                    values=['x', 'y'],
+                                                    aggfunc='first')
+
+        # Flatten the multi-level column index
+        reshaped_dataset.columns = [f'{col[1]}_{col[0]}' for col in reshaped_dataset.columns]
+
+        # Reset the index to have 'timestamp' as a regular column
+        reshaped_dataset = reshaped_dataset.fillna(-1)
+        reshaped_dataset = reshaped_dataset.reindex(sorted(reshaped_dataset.columns), axis=1)
+        reshaped_dataset = reshaped_dataset.reset_index()
+        reshaped_dataset.to_csv( path + "test_dynammic_agents.csv")
+
+
+        history_length = self.history_length
+        prediction_length = self.prediction_length
+        
+        total_sequence_length = history_length + prediction_length
+
+        train_dataset = []
+        for pid, group in train_raw_dataset.groupby("id"):
+            timestamps = group["timestamp"].values
+            x_values = group["x"].values
+            y_values = group["y"].values
+            for i in range(len(group) - total_sequence_length + 1):
+                # Extract the sequence and target
+                sequence = np.column_stack((x_values[i:i+history_length], y_values[i:i+history_length]))
+                target = np.column_stack((x_values[i+history_length:i+total_sequence_length], y_values[i+history_length:i+total_sequence_length]))
+                train_dataset.append((torch.tensor(sequence, dtype=torch.float32), torch.tensor(target, dtype=torch.float32)))
+
+        test_dataset = []
+        for pid, group in validation_raw_dataset.groupby("id"):
+            timestamps = group["timestamp"].values
+            x_values = group["x"].values
+            y_values = group["y"].values
+            for i in range(len(group) - total_sequence_length + 1):
+                # Extract the sequence and target
+                sequence = np.column_stack((x_values[i:i+history_length], y_values[i:i+history_length]))
+                target = np.column_stack((x_values[i+history_length:i+total_sequence_length], y_values[i+history_length:i+total_sequence_length]))
+                test_dataset.append((torch.tensor(sequence, dtype=torch.float32), torch.tensor(target, dtype=torch.float32)))
+    
+        with open(path + 'train_dataset.pickle', 'wb') as handle:
+            pickle.dump(train_dataset, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+        with open(path + 'test_dataset.pickle', 'wb') as handle:
+            pickle.dump(test_dataset, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+    def train(self, LSTM_filename):
+        with open(self.path + 'train_dataset.pickle', 'rb') as handle:
+            train_dataset = pickle.load(handle)
+
+        with open(self.path + 'test_dataset.pickle', 'rb') as handle:
+            test_dataset = pickle.load(handle)
+        
+        # Convert to PyTorch DataLoader
+        train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
+        test_loader = DataLoader(test_dataset, batch_size=64, shuffle=False)
+
+        history_length = self.history_length
+        prediction_length = self.prediction_length
+
+        path = self.path
+        LSTM_file = path + LSTM_filename
+        input_size = 2  # Assuming 2 features (x, y) per timestep
+        hidden_size = 64
+        output_size = 2  # Assuming 2 features in the output
         # Create an instance of the model
-        pass
+        model = LSTMModel(input_size, hidden_size, output_size * prediction_length)
+        device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+        model.to(device)
 
-    def load_trained_model(self, LSTM_file):
-        self.model = LSTMModel(input_size, hidden_size, output_size * prediction_length)        
-        # Check if GPU is available
-        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        # Load the saved model weights
-        self.model.load_state_dict(torch.load(LSTM_file, map_location=device))
-        # Move the model to the GPU if available
-        self.model.to(device)
+        # Define loss function and optimizer
+        criterion = nn.MSELoss()
+        optimizer = optim.Adam(model.parameters(), lr=0.001)
+        num_epochs = 10
+        # Assuming 'train_loader' is your DataLoader
+        for epoch in range(num_epochs):
+            for batch_input, batch_target in train_loader:
+                # Forward pass
+                batch_input, batch_target = batch_input.to(device), batch_target.to(device)
 
-    def predict(self, test):
-        model = self.model
-        x = torch.tensor(test, dtype=torch.float32).clone().detach().unsqueeze(0)  # Add batch dimension
-        # Ensure the model is in evaluation mode
-        model.eval()
-        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+                output = model(batch_input)
+                output = output.view(-1, prediction_length, output_size)
+                # Compute the loss
+                loss = criterion(output, batch_target)
+                # Backward pass and optimization
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+            # Print the loss after each epoch
+            print(f'Epoch [{epoch + 1}/{num_epochs}], Loss: {loss.item():.4f}')
+        # Training complete
+
+        model_metadata = {
+        'input_size': input_size,
+        'hidden_size': hidden_size,
+        'output_size': output_size ,
+        'prediction_length': prediction_length,
+        'history_length': history_length,
+        'state_dict': model.state_dict()
+        }
+        torch.save(model_metadata, LSTM_file)
+        print("model saved")
+
+class Predictor():
+    def __init__(self, model_file):
+        # Create an instance of the model
+        # history_length = 0, prediction_legnth = 0, input_size = 0)
+        loaded_model = torch.load(model_file)
+        self.input_size = loaded_model['input_size']
+        self.hidden_size = loaded_model['hidden_size']
+        self.output_size  = loaded_model['output_size']
+        self.prediction_length = loaded_model['output_size']
+        self.history_length = loaded_model['history_length']
+        self.model = LSTMModel(self.input_size, self.hidden_size, self.output_size * self.prediction_length)
+        self.model.load_state_dict(loaded_model['state_dict'])
+        print("history length", self.history_length, "prediction length", self.prediction_length)
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    
+    def _predict(self, x):
+        self.model.eval()
         with torch.no_grad():
-            x = x.to(device)
-            model_output = model(x)
-        model_output = model_output.view(-1, prediction_length, output_size)
-        predicted_values = model_output[0, -prediction_length:, :].cpu().numpy()  # Move back to CPU for further processing if needed
+            x = x.to(self.device)
+            model_output = self.model(x)
+        model_output = model_output.view(-1, self.prediction_length, self.output_size)
+        predicted_values = model_output[0, -self.prediction_length:, :].cpu().numpy()  # Move back to CPU for further processing if needed
         return predicted_values
+
+    def get_observations_from_moving_agents(self, dynamic_agents, cur_time):
+        history_length = self.history_length
+        res = []
+        for i in range(2, len(dynamic_agents.columns), 2):
+            values = dynamic_agents.loc[cur_time: cur_time + history_length, [dynamic_agents.columns[i], dynamic_agents.columns[i+1]]]
+            values = np.array(values)
+            res.append(values)
+        res = np.array(res)
+        res = torch.tensor(res, dtype=torch.float32)
+        return res
+
+    def predit_in_batch(self, dynamic_agents, cur_time):
+        obs = self.get_observations_from_moving_agents(dynamic_agents, cur_time)
+        return self._predict(obs)
+    
+    def predict(self, dynamic_agents, cur_time):
+        history_length = self.history_length
+        res = []
+        for i in range(2, len(dynamic_agents.columns), 2):
+            values = dynamic_agents.loc[cur_time: cur_time + history_length, [dynamic_agents.columns[i], dynamic_agents.columns[i+1]]]
+            test = torch.tensor(np.array(values), dtype=torch.float32).clone().detach().unsqueeze(0)  # Add batch dimension
+            p = self._predict(test)
+            res.append(p)
+        return res
 
     def conformal_prediction(self, horizon):
         ACP = [()] * horizon
         return ACP
 
+
 if __name__ == "__main__":
+    path = './OpenTraj/datasets/ETH/seq_eth/'
+    raw_file = 'obsmat.txt'
+    lstm_model = 'model_weights.pth'
+    # d = TrainModel(path, raw_file)
+    # d.preprocess()
+    # d.train()
 
-    file = path + "obsmat.txt"
-    csv_columns = ["frame_id", "id", "x", "z", "y", "vel_x", "vel_z", "vel_y"]
-    # read from csv => fill traj table
-    raw_dataset = pd.read_csv(file, sep=r"\s+", header=None, names=csv_columns)
-    raw_dataset["timestamp"]= raw_dataset.frame_id
-    start_frame = raw_dataset.frame_id.min()
-    d_frame = np.diff(pd.unique(raw_dataset["frame_id"]))
-    fps = d_frame[0] * 2.5  # 2.5 is the common annotation
-    for i in raw_dataset.index:
-        raw_dataset.loc[i, "timestamp"] = (raw_dataset.loc[i, "frame_id"] - start_frame) / fps
-    raw_dataset = raw_dataset.loc[:, ["timestamp", "id", "x", "y"]]
-
-    train_id, test_id = train_test_split(raw_dataset.id.unique(), test_size= 1 / 10, random_state=42)
-    train_id, test_id = set(train_id), set(test_id)
-
-    min_lenth = 10
-    total_sequence_length = history_length + prediction_length
-
-    train_dataset = []
-    test_dataset = []
-    for pid, group in raw_dataset.groupby("id"):
-        timestamps = group["timestamp"].values
-        x_values = group["x"].values
-        y_values = group["y"].values
-
-        for i in range(len(group) - total_sequence_length + 1):
-            # Extract the sequence and target
-            sequence = np.column_stack((x_values[i:i+history_length], y_values[i:i+history_length]))
-            target = np.column_stack((x_values[i+history_length:i+total_sequence_length], y_values[i+history_length:i+total_sequence_length]))
-            if pid in train_id:
-                train_dataset.append((torch.tensor(sequence, dtype=torch.float32), torch.tensor(target, dtype=torch.float32)))
-            else:
-                test_dataset.append((torch.tensor(sequence, dtype=torch.float32), torch.tensor(target, dtype=torch.float32)))
-
-    # Convert to PyTorch DataLoader
-    train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
-    test_loader = DataLoader(test_dataset, batch_size=64, shuffle=False)
-
-    input_size = 2  # Assuming 2 features (x, y) per timestep
-    hidden_size = 64
-    output_size = 2  # Assuming 2 features in the output
-    # Create an instance of the model
-    model = LSTMModel(input_size, hidden_size, output_size * prediction_length)
-
-    device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
-    model.to(device)
-
-    # Define loss function and optimizer
-    criterion = nn.MSELoss()
-    optimizer = optim.Adam(model.parameters(), lr=0.001)
-    num_epochs = 10
-    # Assuming 'train_loader' is your DataLoader
-    for epoch in range(num_epochs):
-        for batch_input, batch_target in train_loader:
-            # Forward pass
-            batch_input, batch_target = batch_input.to(device), batch_target.to(device)
-
-            output = model(batch_input)
-            output = output.view(-1, prediction_length, output_size)
-            # Compute the loss
-            loss = criterion(output, batch_target)
-            # Backward pass and optimization
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-        # Print the loss after each epoch
-        print(f'Epoch [{epoch + 1}/{num_epochs}], Loss: {loss.item():.4f}')
-    # Training complete
-
-    torch.save(model.state_dict(), LSTM_file)
-
-    pred = Predictor()
-    test_a = [[7.4008, 5.3609],
-         [6.9278, 5.4221],
-         [6.6198, 5.3929],
-         [6.1320, 5.3706],
-         [5.5926, 5.3497]]
-    res = pred.predict(test_a)
-    print(res)
+    pred = Predictor(path + lstm_model)
+    dynamic_agents =  pd.read_csv(path + "test_dynammic_agents.csv")
+    t1 = time.time()
+    for cur_time in range(40):
+        p = pred.predit_in_batch(dynamic_agents, cur_time)
+    t2 = time.time()
+    for cur_time in range(40):
+        p = pred.predict(dynamic_agents, cur_time)
+    t3 = time.time()
+    print(t2-t1, t3 -t2)
